32710d0c2c4f2d228ab397fe074bb8b62818c554
Damian Momot <damianmomot@google.com>
2026-05-19 09:41:44 -0700

    chore: mark version as 0.1.1-SNAPSHOT
    PiperOrigin-RevId: 917881454

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ val androidMinSdk = providers.gradleProperty("androidMinSdk").getOrElse("26").to
 
 allprojects {
   group = "com.google.adk"
-  version = "0.1.0"
+  version = "0.1.1-SNAPSHOT"
 
   repositories {
     mavenCentral()
--- core/src/commonMain/kotlin/com/google/adk/kt/Version.kt
+++ core/src/commonMain/kotlin/com/google/adk/kt/Version.kt
@@ -15,4 +15,4 @@
  */
 package com.google.adk.kt
 
-const val VERSION = "0.1.0"
+const val VERSION = "0.1.1-SNAPSHOT"
